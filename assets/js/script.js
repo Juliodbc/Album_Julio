e68ffe3ec867aaaea0b992a48a24b333ea8ab522
Dominiick1404 <deborbajulio@gmail.com>
2024-11-06 16:18:46 -0300

    Nosso_Album

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,10 @@
-function toggleImage() {
-    const container = document.getElementById("imageContainer");
-    container.classList.toggle("expanded");
-  }
+document.addEventListener('DOMContentLoaded', function () {
+  const buttons = document.querySelectorAll('.btn');
+
+  buttons.forEach(button => {
+      button.addEventListener('click', function (event) {
+          event.preventDefault();
+          alert('Detalhes do jogo ainda não disponíveis!');
+      });
+  });
+});
